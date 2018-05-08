a33ee270f1952242c7f43b9f5ea71b5199b9a1ec
Burqueño <YoungEagle@MacBook-Pro.local>
2018-05-08 18:52:07 -0400

    new files

--- main.js
+++ main.js
@@ -15,3 +15,24 @@ $("#card7").flip();
 $("#card8").flip();
 
 $("#card9").flip();
+
+
+var card = document.querySelector(".card");
+var playing = false;
+
+card.addEventListener('click',function() {
+  if(playing)
+    return;
+
+  playing = true;
+  anime({
+    targets: card,
+    scale: [{value: 1}, {value: 1.4}, {value: 1, delay: 250}],
+    rotateY: {value: '+=180', delay: 200},
+    easing: 'easeInOutSine',
+    duration: 400,
+    complete: function(anim){
+       playing = false;
+    }
+  });
+});
